6cc0965eda8a9ead29a532321c440c4ca8b9af7c
David Shaw <dshaw11@utk.edu>
2024-06-25 08:45:14 -0400

    served up the index.html information

--- main.go
+++ main.go
@@ -5,20 +5,22 @@ import (
 	"log"
 )
 
-type Server struct {}
+// type Server struct {}
 
-func (Server) ServeHTTP(http.ResponseWriter, *http.Request) {}
+// func (Server) ServeHTTP(http.ResponseWriter, *http.Request) {}
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
