df66e9914dd38d9ad5a3e82cd4ba54c9107cb7c3
David Shaw <dshaw11@utk.edu>
2024-06-26 16:16:59 -0400

    added a counter state manager and a reset count rout

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"net/http"
 	"log"
+	"fmt"
 )
 
 // type Server struct {}
+type apiConfig struct {
+	fileserverHits int
+}
+
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -13,15 +18,37 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) middlewareMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits)
+}
+func (cfg *apiConfig) middlewareResetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	const filepathRoot = "."
-	// const assetPath = "/assets"
 	const port = "8080"
 
+	apiCfg := &apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.Handle("/assets", http.FileServer(http.Dir(filepathRoot)))
 	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.middlewareMetricsHandler)	
+	mux.HandleFunc("/reset", apiCfg.middlewareResetHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
