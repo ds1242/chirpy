2fc11597fa1e5ee66670ba7521a4abc7df39d449
David Shaw <dshaw11@utk.edu>
2024-06-27 12:02:43 -0400

    setup initial chirp validator route

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"net/http"
-	"log"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 // type Server struct {}
 type apiConfig struct {
 	fileserverHits int
 }
-
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -21,8 +21,16 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 func (cfg *apiConfig) middlewareMetricsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits)
+	fmt.Fprintf(w, `<html>
+<body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %d times!</p>
+</body>
+
+</html>
+`, cfg.fileserverHits)
 }
+
 func (cfg *apiConfig) middlewareResetHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits = 0
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -37,6 +45,24 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
+func chirpValidator(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	fmt.Printf(params.Body)
+}
+
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
@@ -47,9 +73,9 @@ func main() {
 	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.Handle("/assets", http.FileServer(http.Dir(filepathRoot)))
 	mux.HandleFunc("GET /api/healthz", healthzHandler)
-	mux.HandleFunc("GET /admin/metrics", apiCfg.middlewareMetricsHandler)	
+	mux.HandleFunc("GET /admin/metrics", apiCfg.middlewareMetricsHandler)
 	mux.HandleFunc("/api/reset", apiCfg.middlewareResetHandler)
-
+	mux.HandleFunc("/api/validate_chirp", chirpValidator)
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
