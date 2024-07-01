1c382ec9300288fd5f1645a0e1aa9a01358de309
David Shaw <dshaw11@utk.edu>
2024-07-01 16:35:02 -0400

    added helper functions and got them setup correctly

--- lenValidator.go
+++ lenValidator.go
@@ -3,7 +3,6 @@ package main
 import (
 	"net/http"
 	"encoding/json"
-	"log"
 )
 
 
@@ -13,44 +12,23 @@ func chirpValidator(w http.ResponseWriter, r *http.Request) {
 		Body string `json:"body"`
 	}
 
-	
-
 	type validChirp struct {
 		Valid bool `json:"valid"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
-
 	err := decoder.Decode(&params)
 	if err != nil {
-		log.Printf("Error decoding parameters: %s", err)
-		w.WriteHeader(500)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
 		return
 	}
 	if len(params.Body) > 140 {
-		dat, err := json.Marshal(invalidChirp{Error:"Chirp is too long"})
-
-		if err != nil {
-			log.Printf("Error marshalling JSON: %s", err)
-			w.WriteHeader(500)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(400)
-		w.Write(dat)
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return 
 	}
 
-	dat, err := json.Marshal(validChirp{Valid: true})
-
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write(dat)
+	respondWithJSON(w, http.StatusOK, validChirp{
+		Valid: true,
+	})
 }
